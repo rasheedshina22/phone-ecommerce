f4766df723e9c1f3bf103c426bd29ca8b049dc38
shina <rasheedshinaopeyemi@gmail.com>
2020-07-18 17:16:53 +0100

    initial project setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+import { FaCartPlus } from 'react-icons/fa';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+        {/*  https://www.iconfinder.com/icons/1243689/call_phone_icon Creative
+        Commons (Attribution 3.0 Unported);
+        https://www.iconfinder.com/Makoto_msk */}
+        <Link to='/' className='navbar-brand'>
+          <img src={logo} alt='logo' />
+        </Link>
+        <ul className='navbar-nav align-items-center'>
+          <li className='nav-item ml-5'>
+            <Link to='/' className='nav-link'>
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to='/cart' className='ml-auto'>
+          <button>
+            <FaCartPlus />
+            cart
+          </button>
+        </Link>
+      </nav>
+    );
+  }
+}
